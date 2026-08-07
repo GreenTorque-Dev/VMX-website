d08c855fbb4cbd3660622cbb224accdec6e12ebb
greentorque <m.fouzan@gt-kw.com>
2026-08-07 15:32:49 +0530

    fixed seo errors

--- contact.php
+++ contact.php
@@ -3,19 +3,19 @@
 	/* ==========================  Define variables ========================== */
 
 	#Your e-mail address
-	define("__TO__", "a.bublaku@gmail.com");
+	define("__TO__", "support@greencloud.live");
 
 	#Message subject
-	define("__SUBJECT__", "");
+	define("__SUBJECT__", "VMX Website Contact Form:");
 
 	#Success message
 	define('__SUCCESS_MESSAGE__', "Your message has been sent. Thank you!");
 
 	#Error message 
-	define('__ERROR_MESSAGE__', "Error, your message hasn't been sent");
+	define('__ERROR_MESSAGE__', "Error, your message could not be sent.");
 
-	#Messege when one or more fields are empty
-	define('__MESSAGE_EMPTY_FILDS__', "Please fill out  all fields");
+	#Message when one or more fields are empty
+	define('__MESSAGE_EMPTY_FIELDS__', "Please fill out all required fields.");
 
 	/* ========================  End Define variables ======================== */
 
@@ -30,55 +30,64 @@
 
 	//Check e-mail validation
 	function check_email($email){
-		if(!@eregi("^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", $email)){
-			return false;
-		} else {
-			return true;
-		}
+		return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
 	}
 
 	//Get post data
-	if(isset($_POST['name']) and isset($_POST['mail']) and isset($_POST['comment'])){
-		$name 	 = $_POST['name'];
-		$mail 	 = $_POST['mail'];
-		$website  = $_POST['website'];
-		$comment = $_POST['comment'];
+	if(isset($_POST['first_name']) and isset($_POST['last_name']) and isset($_POST['email']) and isset($_POST['message'])){
+		$first_name = trim($_POST['first_name']);
+		$last_name  = trim($_POST['last_name']);
+		$email      = trim($_POST['email']);
+		$company    = isset($_POST['company']) ? trim($_POST['company']) : '';
+		$message    = trim($_POST['message']);
 
-		if($name == '') {
-			echo json_encode(array('info' => 'error', 'msg' => "Please enter your name."));
+		if($first_name == '') {
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter your first name."));
 			exit();
-		} else if($mail == '' or check_email($mail) == false){
-			echo json_encode(array('info' => 'error', 'msg' => "Please enter valid e-mail."));
+		} else if($last_name == '') {
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter your last name."));
 			exit();
-		} else if($comment == ''){
+		} else if($email == '' or check_email($email) == false){
+			echo json_encode(array('info' => 'error', 'msg' => "Please enter a valid e-mail."));
+			exit();
+		} else if($message == ''){
 			echo json_encode(array('info' => 'error', 'msg' => "Please enter your message."));
 			exit();
 		} else {
 			//Send Mail
 			$to = __TO__;
-			$subject = __SUBJECT__ . ' ' . $name;
-			$message = '
+			$fullName = $first_name . ' ' . $last_name;
+			$subject = __SUBJECT__ . ' ' . $fullName;
+			
+			$email_content = '
 			<html>
 			<head>
-			  <title>Mail from '. $name .'</title>
+			  <title>Mail from '. htmlspecialchars($fullName) .'</title>
 			</head>
 			<body>
-			  <table style="width: 500px; font-family: arial; font-size: 14px;" border="1">
-				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">Name:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $name .'</td>
+			  <table style="width: 500px; font-family: arial, sans-serif; font-size: 14px; border-collapse: collapse; border: 1px solid #ddd;" border="1" cellpadding="8">
+				<tr style="background-color: #f2f2f2; height: 32px;">
+				  <th colspan="2" align="left">Contact Inquiry Details</th>
 				</tr>
 				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">E-mail:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $mail .'</td>
+				  <th align="right" style="width:150px; font-weight: bold;">First Name:</th>
+				  <td align="left">'. htmlspecialchars($first_name) .'</td>
 				</tr>
 				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">subject:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $subject .'</td>
+				  <th align="right" style="font-weight: bold;">Last Name:</th>
+				  <td align="left">'. htmlspecialchars($last_name) .'</td>
 				</tr>
 				<tr style="height: 32px;">
-				  <th align="right" style="width:150px; padding-right:5px;">Comment:</th>
-				  <td align="left" style="padding-left:5px; line-height: 20px;">'. $comment .'</td>
+				  <th align="right" style="font-weight: bold;">E-mail:</th>
+				  <td align="left"><a href="mailto:'. htmlspecialchars($email) .'">'. htmlspecialchars($email) .'</a></td>
+				</tr>
+				<tr style="height: 32px;">
+				  <th align="right" style="font-weight: bold;">Company:</th>
+				  <td align="left">'. ($company !== '' ? htmlspecialchars($company) : 'N/A') .'</td>
+				</tr>
+				<tr>
+				  <th align="right" valign="top" style="font-weight: bold;">Message:</th>
+				  <td align="left" style="line-height: 1.5; white-space: pre-wrap;">'. htmlspecialchars($message) .'</td>
 				</tr>
 			  </table>
 			</body>
@@ -87,11 +96,12 @@
 
 			$headers  = 'MIME-Version: 1.0' . "\r\n";
 			$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-			$headers .= 'From: ' . $mail . "\r\n";
+			$headers .= 'From: ' . $email . "\r\n";
+			$headers .= 'Reply-To: ' . $email . "\r\n";
 
-			send_mail($to,$subject,$message,$headers);
+			send_mail($to,$subject,$email_content,$headers);
 		}
 	} else {
-		echo json_encode(array('info' => 'error', 'msg' => __MESSAGE_EMPTY_FILDS__));
+		echo json_encode(array('info' => 'error', 'msg' => __MESSAGE_EMPTY_FIELDS__));
 	}
  ?>
